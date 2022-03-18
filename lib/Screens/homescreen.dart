f2383ee12fe251b61549c1f1f22144f3c352b067
lennie-ein <lenniemaina05@gmail.com>
2022-03-18 22:01:32 +0300

    Blur to background

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
               height: 1320,
             ),
             blur: 8.0,
-            blurColor: Colors.black,
+            blurColor: Colors.black54,
           )
         ],
       ),
